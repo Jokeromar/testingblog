10b1105325a6d2cc76975db954a8b5632317d65a
OMAR EZZAROUALI <55711003+Jokeromar@users.noreply.github.com>
2022-09-14 20:33:18 +0100

    Update main.js

--- Java/main.js
+++ Java/main.js
@@ -20,3 +20,25 @@ let header = document.querySelector('header');
 window.addEventListener('scroll',()=>{
     header.classList.toggle('shadow',window.scrollY > 0);
 })
+const telegram = document.querySelector('.telegram');
+const twitter = document.querySelector('.twitter');
+const whatsapp = document.querySelector('.whatsapp');
+const hrl= location.href;
+const message= 'This website Blog have great articles on features technologies';
+const telegramAPI = "https://t.me/share/url?url={https://jokeromar.github.io/testingblog/}&text={This website Blog have great articles on features technologies}";
+const twitterAPI =   'https://twitter.com/intent/tweet?text=This website Blog have great articles on features technologies https://jokeromar.github.io/testingblog/' ;
+const whatsappAPI = 'whatsapp://send?text=This website Blog have great articles on features technologies https://jokeromar.github.io/testingblog/';
+
+telegram.addEventListener('click',()=>{
+    window.open(url=telegramAPI,target='blank')
+});
+
+twitter.addEventListener('click',()=>{
+    window.open(url=twitterAPI,target='blank')
+
+});
+
+whatsapp.addEventListener('click',()=>{
+    window.open(url=whatsappAPI,target='blank')
+
+});
